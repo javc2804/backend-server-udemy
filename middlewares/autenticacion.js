25e0fd2e607fb154d605eb2152759cfad22aaea1
javier villarroel <javier.villarroel@proyectokamila.com>
2019-08-13 07:22:19 -0500

    fin seccion 10

---
+++ middlewares/autenticacion.js
@@ -0,0 +1,21 @@
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED
+
+exports.verificaToken = function (req, res, next) {
+    // Verificar token 
+    
+    var token = req.query.token;
+    jwt.verify( token, SEED, ( err, decoded ) => {
+        if ( err ) {
+            return res.status(401).json({
+                ok: false,
+                msj: 'Token incorrecto',
+                errors: err
+            });
+        }
+
+        req.usuario = decoded.usuario;
+
+        next();
+    });
+}
